5b00f4618987aadd2af5ab3324a0621f4c6009e8
Nikos127 <119165557+Nikos127@users.noreply.github.com>
2026-01-07 15:49:43 +0100

    overlay testing

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let fotoTitles = [
-    blue - 115762_1280,
-    blue - cave - 590336_1280,
-    chair - 2736513_1280,
-    greece - 6821802_1280,
-    island - 203153_1280,
-    mykonos - 5181484_1280,
-    rhodes - 2088130_1280,
-    santorin - 1700274_1280,
-    santorin - 1788606_1280,
-    santorini - 169239_1280,
-    santorini - 86815_1280,
-    zakynthos - 1583095_1280,
+    "blue - 115762_1280",
+    "blue - cave - 590336_1280",
+    "chair - 2736513_1280",
+    "greece - 6821802_1280",
+    "island - 203153_1280",
+    "mykonos - 5181484_1280",
+    "rhodes - 2088130_1280",
+    "santorin - 1700274_1280",
+    "santorin - 1788606_1280",
+    "santorini - 169239_1280",
+    "santorini - 86815_1280",
+    "zakynthos - 1583095_1280"
 ]
 
 function resp_overlay(event) {
@@ -18,8 +18,10 @@ function resp_overlay(event) {
     document.getElementById('cloudy').classList.add('cloudy');
     event.stopPropagation();
 
+    document.getElementById('resp_image').innerHTML = ``;
     for (i = 0; i < fotoTitles.length; i++) {
-        document.getElementById('resp_image').innerHTML = ${fotoTitles[i]}
+        document.getElementById('resp_image').innerHTML += `<li>${fotoTitles[i]}</li>`
+        document.getElementById('resp_image').innerHTML += `<img src="./img/${fotoTitles[i]}.jpg" alt="">`
     }
 }
 
